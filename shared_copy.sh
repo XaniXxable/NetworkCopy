1129dd81e2faf22ce32c3e002ef8456ec29d9cdf
Hemmer, Matthias AVL/AT <matthias.hemmer@avl.com>
2021-06-21 10:48:52 +0200

    Init simple file copy script

---
+++ shared_copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+net_dir=$NET_DIR
+filename=`basename "$0"`
+copy_file_path=$1
+
+# Check the parametr count.
+if [ $# != 1 ]; then
+  printf "Usage: ./$filename <path to file>";
+  exit -2;
+fi
+
+# Check if the network drive has been already mounted.
+if mountpoint -q -- "$net_dir"; then
+  printf "Copy file $copy_file_path";
+  sudo cp $copy_file_path $net_dir;
+else
+  printf "$net_dir is not mounted yet!";
+  exit -1;
+fi
